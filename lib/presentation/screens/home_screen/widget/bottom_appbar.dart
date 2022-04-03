65e2c7a62d68392f14b0f1ef6f59f3b8268245a6
denihero <denihero81@gmail.com>
2022-04-03 11:39:11 +0600

    little fix,and add border

--- lib/presentation/screens/home_screen/widget/bottom_appbar.dart
+++ lib/presentation/screens/home_screen/widget/bottom_appbar.dart
@@ -9,26 +9,45 @@ class Bottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home,size: 30,),
-          label: '',
+    return Container(
+      height: 70,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(21),
+            topRight: Radius.circular(21)),
+        boxShadow: [
+          BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(21),
+            topRight: Radius.circular(21)),
+
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedIconTheme: const IconThemeData(color: BLUE),
+          unselectedItemColor: BLACK,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home,size: 30,),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,size: 30,),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_sharp,size: 30,),
+              label: '',
+            ),
+          ],
+          currentIndex: selectedIndex,
+          selectedItemColor: BLUE,
+          onTap: onTap,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite,size: 30,),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person,size: 30,),
-          label: '',
-        ),
-      ],
-      currentIndex: selectedIndex,
-      selectedItemColor: BLUE,
-      onTap: onTap,
+      ),
     );
   }
 }
