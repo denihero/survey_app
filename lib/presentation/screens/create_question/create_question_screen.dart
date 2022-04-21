57ceb61372fca2b14f8b42d6d724e3a797bef367
denihero <denihero81@gmail.com>
2022-04-21 12:25:42 +0600

    add design to admin screen

--- lib/presentation/screens/create_question/create_question_screen.dart
+++ lib/presentation/screens/create_question/create_question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:survey/core/constants/color.dart';
+import 'package:survey/core/constants/style.dart';
 
 
 class CreateQuestionScreen extends StatelessWidget {
@@ -21,10 +22,107 @@ class CreateQuestionScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const SafeArea(
-        child: Text(
-          'Create Question Screen'
-        ),
+      body: SafeArea(
+        child: Column(
+          children: [
+             Align(
+               alignment: Alignment.topLeft,
+               child: Padding(
+                 padding: EdgeInsets.only(
+                   top: MediaQuery.of(context).size.width * 0.07,
+                   left: MediaQuery.of(context).size.width * 0.06,
+                   bottom: MediaQuery.of(context).size.width * 0.03,
+                 ),
+                 child: Text(
+                    'Write your questions',
+                    style: Monsterats_500_18_FONT_SIZE_BLACK,
+            ),
+               ),
+             ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.06,
+              child: TextFormField(
+                textAlignVertical: TextAlignVertical.bottom,
+                textCapitalization: TextCapitalization.sentences,
+                cursorHeight: 20,
+                style: const TextStyle(
+                  fontSize: 16
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Question',
+                  hintStyle: const TextStyle(
+                    fontSize: 16
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 0.09,
+                  left: MediaQuery.of(context).size.width * 0.06,
+                ),
+                child: Text(
+                  'Possible Answer',
+                  style: Monsterats_500_18_FONT_SIZE_BLACK,
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.06,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 0.020,
+                ),
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.bottom,
+                  textCapitalization: TextCapitalization.sentences,
+                  cursorHeight: 20,
+                  style: const TextStyle(
+                      fontSize: 16
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Answer',
+                    hintStyle: const TextStyle(
+                        fontSize: 16
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+
+          ],
+        )
       ),
     );
   }
