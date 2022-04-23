c6dcb75c2d90e03c06dc8ef610fa46245eb75140
denihero <denihero81@gmail.com>
2022-04-23 15:03:57 +0600

    add and remote func and responsive

--- lib/presentation/screens/create_question/create_question_screen.dart
+++ lib/presentation/screens/create_question/create_question_screen.dart
@@ -3,25 +3,56 @@ import 'package:survey/core/constants/color.dart';
 import 'package:survey/core/constants/style.dart';
 import 'package:survey/presentation/screens/create_question/widget/answer_textformfield.dart';
 import 'package:survey/presentation/screens/create_question/widget/question_formfield.dart';
+import 'package:sizer/sizer.dart';
 
+class CreateQuestionScreen extends StatefulWidget {
+  const CreateQuestionScreen({Key? key}) : super(key: key);
 
-class CreateQuestionScreen extends StatelessWidget {
-  CreateQuestionScreen({Key? key}) : super(key: key);
+  @override
+  State<CreateQuestionScreen> createState() => _CreateQuestionScreenState();
+}
+
+class _CreateQuestionScreenState extends State<CreateQuestionScreen> {
   final List<AnswerTextFormField> answerTextFormField = [];
 
   void addTextFormField() {
+  setState(() {
+    answerTextFormField.add( AnswerTextFormField());
+  });
+  }
+  
+  void deleteCurrentTFF(int index){
+    setState(() {
+      answerTextFormField.removeAt(index);
+    });
 
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: 10.sp,
+            ),
+            child: IconButton(
+                onPressed:() {
+
+                },
+                icon: const Icon(
+                    Icons.check,
+                color:  BLACK ,
+                )),
+          )
+        ],
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Padding(
-          padding: const EdgeInsets.only(
-            top: 10,
+          padding: EdgeInsets.only(
+            top: 10.sp,
           ),
           child: BackButton(
             color: BLACK,
@@ -30,57 +61,65 @@ class CreateQuestionScreen extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-             Align(
-               alignment: Alignment.topLeft,
-               child: Padding(
-                 padding: EdgeInsets.only(
-                   top: MediaQuery.of(context).size.width * 0.07,
-                   left: MediaQuery.of(context).size.width * 0.06,
-                   bottom: MediaQuery.of(context).size.width * 0.03,
-                 ),
-                 child: Text(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 10.w,
+                    left: 2.5.h,
+                    bottom: 3.w,
+                  ),
+                  child: Text(
                     'Write your questions',
                     style: Monsterats_500_18_FONT_SIZE_BLACK,
-            ),
-               ),
-             ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.06,
-              child: const QuestionTextFormField(),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.width * 0.09,
-                  left: MediaQuery.of(context).size.width * 0.06,
-                ),
-                child: Text(
-                  'Possible Answer',
-                  style: Monsterats_500_18_FONT_SIZE_BLACK,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.06,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.width * 0.020,
-                ),
-                child:ListView.builder(
-                  itemCount: answerTextFormField.length,
-                  itemBuilder: (context, index) {
-                    return const AnswerTextFormField();
-                  }),
+              SizedBox(
+                width: 90.w,
+                height: 6.h,
+                child: const QuestionTextFormField(),
               ),
-            ),
-          ],
-        )
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 8.w,
+                    left: 2.5.h,
+                  ),
+                  child: Text(
+                    'Possible Answer',
+                    style: Monsterats_500_18_FONT_SIZE_BLACK,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: answerTextFormField.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          const AnswerTextFormField(),
+                          IconButton(
+                              icon: const Icon(Icons.remove_circle,color: Colors.red,),
+                              onPressed: () => deleteCurrentTFF(index)
+                          )
+                        ],
+                      );
+                    }),
+              ),
+
+              ElevatedButton(
+                  onPressed: () => addTextFormField(),
+                  child: const Text('add')
+              ),
+
+            ],
+          )
       ),
     );
   }
 }
+
--- lib/presentation/screens/create_question/widget/answer_textformfield.dart
+++ lib/presentation/screens/create_question/widget/answer_textformfield.dart
@@ -1,35 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class AnswerTextFormField extends StatelessWidget {
   const AnswerTextFormField({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return  TextFormField(
-      textAlignVertical: TextAlignVertical.bottom,
-      textCapitalization: TextCapitalization.sentences,
-      cursorHeight: 20,
-      style: const TextStyle(
-          fontSize: 16
+    return  Container(
+      height: 8.h,
+      padding: EdgeInsets.symmetric(
+          horizontal: 5.w,
+          vertical: 1.h,
       ),
-      decoration: InputDecoration(
-        hintText: 'Answer',
-        hintStyle: const TextStyle(
-            fontSize: 16
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: Colors.blue,
+      child: Row(
+        children: [
+          Flexible(
+            child: TextFormField(
+              textAlignVertical: TextAlignVertical.bottom,
+              textCapitalization: TextCapitalization.sentences,
+              cursorHeight: 20.sp,
+              style: TextStyle(
+                  fontSize: 16.sp
+              ),
+              decoration: InputDecoration(
+                hintText: 'Answer',
+                hintStyle:  TextStyle(
+                    fontSize: 16.sp
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: const BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2.0.sp,
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: Colors.blue,
-            width: 2.0,
+
+          Visibility(
+            child: SizedBox(
+              width: 30.w,
+              child: IconButton(
+                icon: const Icon(Icons.add_circle),
+                color:Colors.green,
+                onPressed: () {},
+              ),
+            ),
           ),
-        ),
+          Visibility(
+            child: SizedBox(
+              width: 30.w,
+              child: IconButton(
+                icon: const Icon(Icons.remove_circle),
+                color:Colors.red,
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
