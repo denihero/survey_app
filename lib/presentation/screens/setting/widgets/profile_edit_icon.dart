cf9b777170508284dfd265e1e6ef880c4928083b
Baitur5 <baibaitur@gmail.com>
2022-04-30 00:45:14 +0600

    Update

--- lib/presentation/screens/setting/widgets/profile_edit_icon.dart
+++ lib/presentation/screens/setting/widgets/profile_edit_icon.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:survey/presentation/screens/profile/widgets/profileicon.dart';
 
+import '../../../../core/constants/style.dart';
 import '../../../../logic/bloc/auth_bloc.dart';
 
 class ProfileEditIcon extends StatelessWidget {
@@ -23,14 +24,50 @@ class ProfileEditIcon extends StatelessWidget {
           ProfileIcon(
             is_settings: true,
           ):
-          CircleAvatar(
-            radius: 43,
-            child: ClipOval(
-              child: CachedNetworkImage(
-                imageUrl: image ,
-                fit: BoxFit.cover,
-                height: 300,
-                width: 300,
+          GestureDetector(
+            onTap: (){
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return WillPopScope(
+                          onWillPop: ()async=>false,
+                          child: Scaffold(
+                            backgroundColor: Colors.white,
+                            appBar: AppBar(
+                              title:  Text("View Image",style: Monsterats_500_18_FONT_SIZE_BLACK,),
+                              backgroundColor: Colors.white,
+                              leading: IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back_ios,
+                                  color: Colors.black,
+                                  size: 30,
+                                ),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                              elevation: 0,
+                            ),
+                            body: Center(
+                              child: CachedNetworkImage(
+                                imageUrl: image,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+            },
+            child: CircleAvatar(
+              radius: 43,
+              child: ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: image ,
+                  fit: BoxFit.cover,
+                  height: 300,
+                  width: 300,
+                ),
               ),
             ),
           ),
